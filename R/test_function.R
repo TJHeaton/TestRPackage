c937e88657a2ddeac11413eafbf1186e0e9ab93c
Sara Al-Assam <sarassam@gmail.com>
2024-01-15 11:59:06 +0000

    Adding documentation and readme

---
+++ R/test_function.R
@@ -0,0 +1,20 @@
+#' A test function
+#'
+#' A description for this function
+#'
+#' Plots the posterior change points
+#'
+#' Add description
+#'
+#' @param x One number
+#' @param y another number
+#'
+#'
+#' @return x multipled by y
+#' @export
+#'
+#' @examples
+#' test_function(x = 5, y = 7)
+test_function <- function(x = 3, y = 4) {
+  return (x * y)
+}
